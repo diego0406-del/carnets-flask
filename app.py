772036bda51ae2286dd9c00c3e21adb0233d87b5
diego0406-del <juandiegonuevo11@gmail.com>
2025-12-05 18:58:56 -0500

    Refactor app.py to improve carnet handling

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template, abort, send_from_directory
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-CARNETS_DIR = "static/carnets"
-
 @app.route("/")
 def index():
-    imagenes = os.listdir(CARNETS_DIR)
-    return render_template("index.html", imagenes=imagenes)
+    carnets = [f for f in os.listdir("static/carnets") if f.endswith((".png", ".jpg", ".jpeg"))]
+    return render_template("index.html", carnets=carnets)
 
-@app.route("/carnet/<name>")
-def show_carnet(name):
-    # Verifica si existe
-    if not os.path.exists(os.path.join(CARNETS_DIR, name)):
-        abort(404)
-    return render_template("show.html", image_name=name)
+@app.route("/carnet/<filename>")
+def mostrar_carnet(filename):
+    return render_template("carnet_view.html", filename=filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
